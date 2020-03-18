f9199865ea34400336721354d4f1aeb06f9eb0e8
Jasper Sandhu <jsand153@ucr.edu>
2020-03-17 19:55:24 -0700

    Cleaned up controller.js and index.html code.

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,109 +1,38 @@
 app.controller('ngSearch', function ($scope, $http) {
     $scope.searchString = null;
+    $scope.response = null;
+    $scope.obj = null;
+
+    //$scope.json = '{"index":"m","query":"Kobe Basketball","result":"goes into here"}';
+    //obj = JSON.parse($scope.json);
+    //$scope.response = obj.result;
 
     $scope.getDataLucene = function (searchString) {
 
         $scope.visible = 'Lucene: '+searchString; //Show user what he last searched
         console.log(searchString);
-        var data = {
-            index: 'l',
-            name: searchString
-        };
-
-        //Convert to json object
-//        var myJSON = JSON.stringify(data);
 
         $http({
             url: '/api/query',
             method: "GET",
             params: {index: "l", query: searchString}
+         }).then(function success(response) {
+            myResponse = response.data;
+            $scope.response = myResponse.result;
          });
     };
 
     $scope.getDataHadoop = function (searchString) {
         $scope.visible = 'Hadoop: '+searchString; //Show user what he last searched
         console.log(searchString);
-        var data = {
-            index: 'm',
-            name: searchString
-        };
 
         $http({
             url: '/api/query',
             method: "GET",
             params: {index: "m", query: searchString}
+         }).then(function success(response) {
+            myResponse = response.data;
+            $scope.response = myResponse.urllist;
          });
     };
-
 });
-
-//app.controller('ngSearch', function ($scope) {
-//    $scope.clkcount = null;
-//
-//    $scope.getDetails = function (searchString) {
-//        //console.log(searchString);
-//        $scope.clkcount = searchString;
-//
-//        $http.put(url, data, config).then(function (response) {
-//            // This function handles success
-//            }, function (response) {
-//                // this function handles error
-//            });
-//
-//    }
-//});
-
-//var app = angular.module('putserviceApp', []);
-//app.controller('putserviceCtrl', function ($scope, $http) {
-//    // Simple Put request example:
-//    var url = 'puturl', data = 'parameters',config='contenttype';
-//    $http.put(url, data, config).then(function (response) {
-//        // This function handles success
-//    }, function (response) {
-//        // this function handles error
-//    });
-//});
-
-
-// Create the search filter
-app.filter('searchFor', function(){
-    // All filters must return a function. The first parameter
-    // is the data that is to be filtered, and the second is an
-    // argument that may be passed with a colon (searchFor:searchString)
-    return function(dataArr, searchString){
-
-        if(!searchString){
-            return dataArr;
-        }
-
-        var result = [];
-        searchString = searchString.toLowerCase();
-
-        // Using the forEach helper method to loop through the array
-        angular.forEach(dataArr, function(item){
-            if(item.name.toLowerCase().indexOf(searchString) !== -1){
-                result.push(item);
-            }
-        });
-        return result;
-    };
-});
-
-// The controller
-function searchController($scope){
-    // The data model. These items would normally be requested via AJAX,
-    // but are hardcoded here for simplicity. See the next example for
-    // tips on using AJAX.
-
-    $scope.movies = [
-        {name:"The Godfather",releaseYr:"1972"},
-        {name:"Inception",releaseYr:"2010"},
-        {name:"Titanic",releaseYr:"1997"},
-        {name:"Thor:Ragnarok",releaseYr:"2017"},
-        {name:"Forest Gump",releaseYr:"1994"},
-        {name:"Star Wars",releaseYr:"1977"},
-        {name:"Justice League",releaseYr:"2017"},
-        {name:"The Dark Knight",releaseYr:"2008"},
-        {name:"Avatar",releaseYr:"2009"},
-        {name:"The Avengers",releaseYr:"2012"}
-    ];}
